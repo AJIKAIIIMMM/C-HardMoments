9d1b747d38d9ae2a5581dd1ae015306392c73d22
KiFa <maratmm31@gmail.com>
2021-12-05 19:48:35 +0300

    Lesson 5

---
+++ Lesson5.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <set>
+
+using namespace std;
+
+template<class FindUnique>
+FindUnique find_unique(FindUnique first, FindUnique last){
+    set<string> uniquewords;
+    int counter = 1;
+    while (first != last) {
+        auto result = uniquewords.insert(*first);
+        if (!result.second) cout << *first << " is not unique" << "\n";
+        else cout << counter++ << "." << *first << "\n";
+        ++first;
+    }
+    return last;
+}
+
+
+int main(){
+    ifstream file("d:\\SomeText.txt");
+    vector<string> words;
+    string word;
+    while (!file.eof())
+        file >> word;
+        words.push_back(word);
+    file.close();
+    for (int i = 0; i < words.size(); i++) {
+        cout << words[i];
+    }
+    find_unique(words.begin(), words.end());
+
+    set<string> sentences;
+    string sentence;
+    cout << "Enter 10 sentences in a row:";
+    while (getline(cin, sentence, '.') || getline(cin, sentence, '?') || 
+        getline(cin, sentence, '!') && sentences.size() < 10)  {
+        sentences.insert(sentence);
+    }
+    return 0;
+}
+
