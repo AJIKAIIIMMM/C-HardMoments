85564cf37c3379778d9e3d128321185b9d948d55
KiFa <maratmm31@gmail.com>
2021-12-10 19:36:22 +0300

    Lesson 5 modified

--- Lesson5.cpp
+++ Lesson5.cpp
@@ -3,43 +3,61 @@
 #include <vector>
 #include <fstream>
 #include <set>
+#include <iterator>
+#include <map>
 
 using namespace std;
 
 template<class FindUnique>
-FindUnique find_unique(FindUnique first, FindUnique last){
+FindUnique find_unique(FindUnique first, FindUnique last) {
     set<string> uniquewords;
     int counter = 1;
     while (first != last) {
         auto result = uniquewords.insert(*first);
         if (!result.second) cout << *first << " is not unique" << "\n";
         else cout << counter++ << "." << *first << "\n";
-        ++first;
+        next(first);
     }
     return last;
 }
 
 
-int main(){
+int main() {
     ifstream file("d:\\SomeText.txt");
     vector<string> words;
     string word;
     while (!file.eof())
         file >> word;
-        words.push_back(word);
+    words.push_back(word);
     file.close();
     for (int i = 0; i < words.size(); i++) {
         cout << words[i];
     }
     find_unique(words.begin(), words.end());
 
-    set<string> sentences;
+    map<int, string> sentences;
     string sentence;
+    int slovo, count = 0;
     cout << "Enter 10 sentences in a row:";
-    while (getline(cin, sentence, '.') || getline(cin, sentence, '?') || 
-        getline(cin, sentence, '!') && sentences.size() < 10)  {
-        sentences.insert(sentence);
+    cin >> sentence;
+    
+    while (getline(cin, sentence, '.') || getline(cin, sentence, '?') ||
+        getline(cin, sentence, '!') && sentences.size() < 10) {
+        int i = 0;
+        while (sentence[i] == ' ' && sentence[i] != '\0')
+            i++;
+        slovo = 0;
+        while (sentence[i] != '\0') {
+            if (sentence[i] != ' ' && slovo == 0)
+            {
+                slovo = 1;
+                count++;
+            }
+            else if (sentence[i] == ' ')
+                slovo = 0;
+            i++;
+        }
+        sentences.insert({ count, sentence });
     }
     return 0;
 }
-
