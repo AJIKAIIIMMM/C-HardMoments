fb9cb859897cd80c13da1495895e08df3692674c
AJIKAIII <maratmm31@gmail.com>
2022-04-23 21:12:07 +0300

    Last directories were added

---
+++ Lesson 4/Lesson4.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+template <typename box>
+void insert_sorted(box& arr, int input) {
+	arr.insert(upper_bound(arr.begin(), arr.end(), input), input);
+}
+
+
+int main(){
+	//First Task
+	int input;
+	vector <int> arr(100);
+	generate(arr.begin(), arr.end(), []() {return (rand() % 1002); });
+	sort(arr.begin(), arr.end());
+	for (const auto b : arr) cout << b << " ";
+	cout << endl;
+	cout << "Enter the number you want to input:";
+	cin >> input;
+	insert_sorted(arr, input);
+	cout << endl;
+	for (const auto b : arr) cout << b << " ";
+	cout << "\n\n";
+
+
+	//Second Task
+	double a = 101;
+	vector <double> real(100);
+	generate(real.begin(), real.end(), [&a]() {return rand() * a / RAND_MAX; });
+	for (const auto b : real) cout << b << " ";
+	cout << "\n\n";
+
+	vector <int> integer(100);
+	copy(real.begin(), real.end(), integer.begin());
+	for (const auto b : integer) cout << b << " ";
+	cout << "\n\n";
+
+	int n = 0;
+	transform(real.begin(), real.end(), real.begin(), [&](double first) { return first - static_cast<double>(integer[n++]); });
+	for (const auto b : real) cout << b << " ";
+	cout << "\n\n";
+
+	transform(real.begin(), real.end(), real.begin(), [](double first) { return first*first; });
+	for (const auto b : real) cout << b << " ";
+	cout << "\n\n";
+
+	double totalerror = 0.0;
+	cout << accumulate(begin(real), end(real), 0.0);
+	cout << totalerror;
+	
+	return 0;
+}
---
+++ Lesson 5/Lesson5.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <set>
+#include <iterator>
+#include <map>
+
+using namespace std;
+
+template<class FindUnique>
+FindUnique find_unique(FindUnique first, FindUnique last) {
+    set<string> uniquewords;
+    int counter = 1;
+    while (first != last) {
+        auto result = uniquewords.insert(*first);
+        if (!result.second) cout << *first << " is not unique" << "\n";
+        else cout << counter++ << "." << *first << "\n";
+        next(first);
+    }
+    return last;
+}
+
+
+int main() {
+    ifstream file("d:\\SomeText.txt");
+    vector<string> words;
+    string word;
+    while (!file.eof())
+        file >> word;
+    words.push_back(word);
+    file.close();
+    for (int i = 0; i < words.size(); i++) {
+        cout << words[i];
+    }
+    find_unique(words.begin(), words.end());
+
+    map<int, string> sentences;
+    string sentence;
+    int slovo, count = 0;
+    cout << "Enter 10 sentences in a row:";
+    cin >> sentence;
+    
+    while (getline(cin, sentence, '.') || getline(cin, sentence, '?') ||
+        getline(cin, sentence, '!') && sentences.size() < 10) {
+        int i = 0;
+        while (sentence[i] == ' ' && sentence[i] != '\0')
+            i++;
+        slovo = 0;
+        while (sentence[i] != '\0') {
+            if (sentence[i] != ' ' && slovo == 0)
+            {
+                slovo = 1;
+                count++;
+            }
+            else if (sentence[i] == ' ')
+                slovo = 0;
+            i++;
+        }
+        sentences.insert({ count, sentence });
+    }
+    return 0;
+}
